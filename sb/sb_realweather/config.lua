7ad0464c7149de8b59a4ac957ec0b88673b3d474
SaltySec <secbysalty@gmail.com>
2025-08-10 16:39:15 -0500

    Changed SB Dir.

--- sb/sb_realweather/config.lua
+++ sb/sb_realweather/config.lua
@@ -1,6 +1,6 @@
 Config = {}
 
-Config.ApiKey = 'be3d2d3be9a472d3d91fb16a0f39f4cf'
+Config.ApiKey = 'YOUR API KEY HERE'
 Config.City = 'Los Angeles'
 Config.CountryCode = 'US'
 Config.UpdateInterval = 60    -- 1 minute
